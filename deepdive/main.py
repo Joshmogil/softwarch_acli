b6b20b7ff7d4a63e4c9d46564777c0cb81385ee2
Josh Mogil <jmogil@pop-os.localdomain>
2023-08-05 16:50:15 -0400

    deepdive

--- deepdive/main.py
+++ deepdive/main.py
@@ -11,6 +11,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+@app.get("/", response_class=HTMLResponse)
+async def render_home_page(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
